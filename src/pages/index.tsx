f88569194e24fcb52a63c59757a8fad167a5dd14
Thiago Moreira <contatothiagomoreira@outlook.com>
2022-04-26 19:13:09 -0300

    🎉 initial settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Dashgo</title>
+        <meta name="description" content="Dashboard" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Wello World!!</h1>
+    </>
+  );
+};
+
+export default Home;
